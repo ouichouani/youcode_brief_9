a0ec76612db343b4c730b6f866baad65949781f2
ouichouani <ouichouaniabdlhakim@gmail.com>
2025-12-25 11:56:26 +0100

    connection class

--- connection/connection.php
+++ connection/connection.php
@@ -1,14 +1,19 @@
 <?php
-    $servername = 'mysql' ; //container name
-    $username = 'root' ;
-    $password = 'ouichouani' ;
-    $databasename = 'youcode_brief_7' ;
 
-    $connection = new mysqli($servername , $username , $password , $databasename) ;
+namespace Connection;
 
-    if($connection->connect_error){
-        die('connection fail : ' . $connection->connect_error);
+class Connection
+{
+    private static $servername = 'mysql'; //container name
+    private static $username = 'root';
+    private static $password = 'ouichouani';
+    private static $databasename = 'youcode_brief_9';
+
+    public static function connect()
+    {
+        $connection = new \mysqli(self::$servername, self::$username, self::$password, self::$databasename);
+        if ($connection->connect_error) die('connection fail : ' . $connection->connect_error);
+        return $connection;
     }
+}
 
-
-    
